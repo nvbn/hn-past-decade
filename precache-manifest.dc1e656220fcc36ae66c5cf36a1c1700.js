eba88e209fea28151fe6e57cd9f1ec92c5b26ade
Vladimir Iakovlev <nvbn.rm@gmail.com>
2020-01-09 01:12:08 +0100

    Updates

---
+++ precache-manifest.dc1e656220fcc36ae66c5cf36a1c1700.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb03e1c38e58bb3e75daddeaf99f59b",
+    "url": "/hn-past-decade/index.html"
+  },
+  {
+    "revision": "152ba34a3d93d1d3bac6",
+    "url": "/hn-past-decade/static/css/main.f54d40b8.chunk.css"
+  },
+  {
+    "revision": "74b74a389122a48e79d6",
+    "url": "/hn-past-decade/static/js/2.aaf9a0b6.chunk.js"
+  },
+  {
+    "revision": "8de9afe6b8cefbf5b8f494a377ccd55e",
+    "url": "/hn-past-decade/static/js/2.aaf9a0b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "152ba34a3d93d1d3bac6",
+    "url": "/hn-past-decade/static/js/main.e68e9bf0.chunk.js"
+  },
+  {
+    "revision": "df766818fa71abec6888",
+    "url": "/hn-past-decade/static/js/runtime-main.57c56104.js"
+  }
+]);
