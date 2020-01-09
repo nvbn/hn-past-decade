a8a156da77d5c40549fe8062bfb825cae077304e
Vladimir Iakovlev <nvbn.rm@gmail.com>
2020-01-09 01:43:56 +0100

    Add link to github to the header

--- src/components/DashboardScreen/Header.tsx
+++ src/components/DashboardScreen/Header.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import useStyles from "./useStyles";
 
 export default () => {
+  const classes = useStyles();
+
   return (
     <AppBar position="relative">
-      <Toolbar>
-        <Typography style={{ fontSize: "2rem" }} variant="h1">
+      <Toolbar className={classes.toolbar}>
+        <Typography className={classes.h1} variant="h1">
           HN Trends of the Past Decade
         </Typography>
+
+        <Button href="https://github.com/nvbn/hn-past-decade">github</Button>
       </Toolbar>
     </AppBar>
   );
--- src/components/DashboardScreen/useStyles.ts
+++ src/components/DashboardScreen/useStyles.ts
@@ -19,4 +19,7 @@ export default makeStyles(theme => ({
     width: "100%",
     height: "100%",
   },
+  toolbar: {
+    flexDirection: "row",
+  },
 }));
